b08d51e09dcb91de6937ad29997b4fb909031313
Calem-Harris <calem.harris@trios.com>
2025-04-15 19:08:55 -0300

    Initial commit

---
+++ main.lua
@@ -0,0 +1,15 @@
+if arg[2] == "debug" then
+    require("lldebugger").start()
+end
+
+
+--Sits at the bottom of our scripts
+local love_errorhandler = love.errorhandler
+
+function love.errorhandler(msg)
+    if lldebugger then
+        error(msg, 2)
+    else
+        return love_errorhandler(msg)
+    end
+end
